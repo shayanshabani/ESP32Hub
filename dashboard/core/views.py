3f1427a673a4d663cb15e5946898e67fb7f680a2
Moeen89 <moein.samadiazad@gmail.com>
2024-09-01 00:03:14 +0330

    fix bug

--- dashboard/core/views.py
+++ dashboard/core/views.py
@@ -12,8 +12,8 @@ class HomePageView(TemplateView):
 
 
 def get_data(request, uid):
-    sensor: Sensor = Device.objects.get(uid=uid)
-    data = sensor.get()
+    sensor: Sensor = Sensor.objects.get(uid=uid)
+    data = sensor.get_data()
     formated_data = [{'x': d.received_at.timestamp() * 1000, 'y': float(d.message)}
                      for d in data]
     return JsonResponse(formated_data, safe=False)
@@ -28,19 +28,19 @@ def get_device_list(request):
 
 
 def turn_on(request, uid):
-    device: BooleanActuator = Device.objects.get(uid=uid)
+    device: BooleanActuator = BooleanActuator.objects.get(uid=uid)
     device.turn_on()
 
 
 def turn_off(request, uid):
-    device: BooleanActuator = Device.objects.get(uid=uid)
+    device: BooleanActuator = BooleanActuator.objects.get(uid=uid)
     device.turn_off()
 
 
 def integer(request, uid):
     data = json.loads(request.body)
     num = data.get('number')
-    device: IntegerActuator = Device.objects.get(uid=uid)
+    device: IntegerActuator = IntegerActuator.objects.get(uid=uid)
     device.send_int(num)
 
 
